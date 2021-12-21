70aa601d53cb83e2ebcdaa6dcc600cd3b1b20843
Nirmal <nirmal.panneerselvam@cognizant.com>
2021-12-21 17:10:01 +0000

    New changes added

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TestingConsole
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World. Nirmal here");
+        }
+    }
+}
